e376ef27b35211ed04c4fbcd45677ad88bed70b3
Tereza Miklóšová <t.miklosova@seznam.cz>
2024-09-07 17:31:55 +0200

    Changed the sending POST

--- assets/sendPost.js
+++ assets/sendPost.js
@@ -7,48 +7,39 @@ const previewLabel = document.querySelector("#previewLabel");
 
 const divForResponse = document.querySelector("#responsePlace");
 
-const csvData = `"podmět";"předmět"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu";",,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu";"http://www.w3.org/2004/02/skos/core#ConceptScheme"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu";"Days of the week"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pondělí";",,,,,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pondělí";"http://www.w3.org/2004/02/skos/core#Concept"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pondělí";"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pondělí";"Monday,Pondělí"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pondělí";"pondělí"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu";","
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu";"Dny v týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/úterý";",,,,,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/úterý";"http://www.w3.org/2004/02/skos/core#Concept"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/úterý";"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/úterý";"Tuesday,Úterý"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/úterý";"úterý"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/středa";",,,,,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/středa";"http://www.w3.org/2004/02/skos/core#Concept"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/středa";"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/středa";"Wednesday,Středa"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/středa";"středa"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/čtvrtek";",,,,,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/čtvrtek";"http://www.w3.org/2004/02/skos/core#Concept"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/čtvrtek";"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/čtvrtek";"Thursday,Čtvrtek"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/čtvrtek";"čtvrtek"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pátek";",,,,,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pátek";"http://www.w3.org/2004/02/skos/core#Concept"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pátek";"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pátek";"Friday,Pátek"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/pátek";"pátek"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/sobota";",,,,,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/sobota";"http://www.w3.org/2004/02/skos/core#Concept"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/sobota";"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/sobota";"Saturday,Sobota"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/sobota";"sobota"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/neděle";",,,,,"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/neděle";"http://www.w3.org/2004/02/skos/core#Concept"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/neděle";"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/neděle";"Neděle,Sunday"
-"https://data.mvcr.gov.cz/zdroj/číselníky/dny-v-týdnu/položky/neděle";"neděle"`;
+document.getElementById('rdfandconfiguration').addEventListener('submit', async function(event) {
+  event.preventDefault();
 
+  const form = event.target;
+  const formData = new FormData(form);
+
+  try {
+      const response = await fetch("https://rdf-to-csvw.onrender.com/rdftocsvw", {
+          method: "POST",
+          body: formData // Let the browser set the content-type
+      });
+
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+
+      const data = await response.blob();
+      const file = new Blob([data], { type: 'application/zip' });
+      const fileURL = URL.createObjectURL(file);
+
+      const anchorTag = document.createElement('a');
+      anchorTag.href = fileURL;
+      anchorTag.target = '_blank';
+      anchorTag.download = 'result.zip'; // Change this if necessary
+      document.body.appendChild(anchorTag);
+      anchorTag.click();
+      document.body.removeChild(anchorTag);
+
+  } catch (e) {
+      console.error(e);
+  }
+});
+/*
 async function sendData() {
   // Associate the FormData object with the form element
   const formData = new FormData(form);
@@ -100,6 +91,7 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   sendData();
 });
+*/
 
 toggleButton.addEventListener('click', function() {
   var content = document.getElementById('toggleContent');
